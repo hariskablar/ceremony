42dd3e4b003feac4b07319bf0cc1a10a07f5a38c
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-12-06 22:09:30 +0100

    changes on home page

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -49,28 +49,28 @@ export default function Countdown({ target }) {
   return (
     <div className='w-full flex justify-center gap-5 mb-10 '>
       <div className='flex flex-col items-center justify-center leading-none'>
-        <span className='text-4xl font-medium text-earth-4 leading-none caligraphy'>
+        <span className='text-4xl font-medium text-beige-6 leading-none caligraphy'>
           {days.toString()}
         </span>
-        <span className='text-earth-4 text-xl leading-none'>dana</span>
+        <span className='text-beige-6 text-xl leading-none'>dana</span>
       </div>
       <div className='flex flex-col items-center justify-center '>
-        <span className='text-4xl font-medium text-earth-4 leading-none caligraphy'>
+        <span className='text-4xl font-medium text-beige-6 leading-none caligraphy'>
           {hours.toString().padStart(2, '0')}
         </span>
-        <span className='text-earth-4 text-xl leading-none'>sati</span>
+        <span className='text-beige-6 text-xl leading-none'>sati</span>
       </div>
       <div className='flex flex-col items-center justify-center leading-none '>
-        <span className='text-4xl font-medium text-earth-4 leading-none caligraphy'>
+        <span className='text-4xl font-medium text-beige-6 leading-none caligraphy'>
           {minutes.toString().padStart(2, '0')}
         </span>
-        <span className='text-earth-4 text-xl leading-none'>minuta</span>
+        <span className='text-beige-6 text-xl leading-none'>minuta</span>
       </div>
       <div className='flex flex-col items-center justify-center leading-none '>
-        <span className='text-4xl font-medium text-earth-4 leading-none caligraphy'>
+        <span className='text-4xl font-medium text-beige-6 leading-none caligraphy'>
           {seconds.toString().padStart(2, '0')}
         </span>
-        <span className='text-earth-4 text-xl leading-none'>sekundi</span>
+        <span className='text-beige-6 text-xl leading-none'>sekundi</span>
       </div>
     </div>
   );
