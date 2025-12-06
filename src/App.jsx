9ff3a4743463671745bf830bbdead770254b7e61
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-12-06 20:55:33 +0100

    popup button fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 2 }}
-        className='relative inset-0 bg-white z-50 pointer-events-none'
+        className='relative inset-0 bg-beige-1 z-50 pointer-events-none'
       >
         <div className='h-svh w-screen overflow-hidden relative'>
           <img src='/bg1.png' className='object-cover h-full ' />
--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -67,7 +67,7 @@ const RSVP = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 2 }}
-      className='relative inset-0  z-50 pointer-events-none'
+      className='relative inset-0'
     >
       <div className='bg-beige-1 h-screen flex flex-col justify-center'>
         <img src='./lines_with_birds.webp' className='mb-10 w-[90%] mx-auto' />
