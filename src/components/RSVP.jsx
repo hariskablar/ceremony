e5c40bb47588f1e0ae10b906f9d128b9e511e79f
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-12-06 20:49:18 +0100

    popup button fix

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -62,54 +62,64 @@ const RSVP = () => {
   };
 
   return (
-    <div className='bg-1 h-screen flex flex-col justify-center'>
-      <img src='./lines_with_birds.webp' className='mb-10 w-[90%] mx-auto' />
-      {isLoading ? (
-        <p className='w-full text-center text-beige-5 text-7xl caligraphy loading-animation'>
-          A & H
-        </p>
-      ) : (
-        <div className='animate-height'>
-          <h2 className='caligraphy text-4xl text-center mb-2 text-beige-7'>
-            Dobrodošli, draga porodico {family?.family_name}
-          </h2>
-          <p className=' text-xl text-center text-beige-6 px-2'>
-            Hvala vam što ste otvorili našu pozivnicu.
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 2 }}
+      className='relative inset-0  z-50 pointer-events-none'
+    >
+      <div className='bg-beige-1 h-screen flex flex-col justify-center'>
+        <img src='./lines_with_birds.webp' className='mb-10 w-[90%] mx-auto' />
+        {isLoading ? (
+          <p className='w-full text-center text-beige-5 text-7xl caligraphy loading-animation'>
+            A & H
           </p>
-          <p className=' text-xl text-center text-beige-6 leading-5 px-2 my-2'>
-            Vaše prisustvo za nas znači mnogo i iskreno bismo se radovali da
-            ovaj poseban dan podijelimo zajedno.
-          </p>
-          <p className=' text-xl text-center text-beige-6 px-2'>
-            Molimo vas ispod potvrdite svoj dolazak.
-          </p>
-          <ul className='mt-5'>
-            {guests?.map((guest) => (
-              <li key={guest.id} className='ml-[35%] mt-3'>
-                <label className='caligraphy text-beige-6 text-2xl '>
-                  <input
-                    type='checkbox'
-                    checked={!!selected[guest.id]}
-                    onChange={() => toggleId(guest.id)}
-                    className='mr-3 accent-beige-5'
-                  />
-                  {guest.full_name}
-                </label>
-              </li>
-            ))}
-          </ul>
-          <button
-            onClick={handleSubmit}
-            className=' text-beige-7 flex mx-auto mt-10 border border-beige-6 px-10 py-1 rounded-md text-2xl'
-          >
-            Potvrdi dolazak
-          </button>
-        </div>
-      )}
+        ) : (
+          <div className='animate-height'>
+            <h2 className='caligraphy text-4xl text-center mb-2 text-beige-7'>
+              Dobrodošli, draga porodico {family?.family_name}
+            </h2>
+            <p className=' text-xl text-center text-beige-6 px-2'>
+              Hvala vam što ste otvorili našu pozivnicu.
+            </p>
+            <p className=' text-xl text-center text-beige-6 leading-5 px-2 my-2'>
+              Vaše prisustvo za nas znači mnogo i iskreno bismo se radovali da
+              ovaj poseban dan podijelimo sa vama.
+            </p>
+            <p className=' text-xl text-center text-beige-6 px-2'>
+              Molimo vas ispod potvrdite svoj dolazak.
+            </p>
+            <ul className='mt-5'>
+              {guests?.map((guest) => (
+                <li key={guest.id} className='ml-[35%] mt-3'>
+                  <label className='caligraphy text-beige-6 text-2xl '>
+                    <input
+                      type='checkbox'
+                      checked={!!selected[guest.id]}
+                      onChange={() => toggleId(guest.id)}
+                      className='mr-3 accent-beige-5'
+                    />
+                    {guest.full_name}
+                  </label>
+                </li>
+              ))}
+            </ul>
+            <button
+              onClick={handleSubmit}
+              className=' text-beige-7 flex mx-auto mt-10 border border-beige-6 px-10 py-1 rounded-md text-2xl'
+            >
+              Potvrdi dolazak
+            </button>
+          </div>
+        )}
 
-      <img src='./without_birds.webp' className='mt-10 w-[90%] mx-auto' />
-      {showPopup && <PopUp showModal={showPopup} setShowPopup={setShowPopup} />}
-    </div>
+        <img src='./without_birds.webp' className='mt-10 w-[90%] mx-auto' />
+        {showPopup && (
+          <PopUp showModal={showPopup} setShowPopup={setShowPopup} />
+        )}
+      </div>
+    </motion.div>
   );
 };
 
