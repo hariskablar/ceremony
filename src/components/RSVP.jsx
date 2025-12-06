d43564f8934fc6a4d2cbab6446c3dad4056e2489
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-12-06 17:50:07 +0100

    loading state in rsvp

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 function useQuery() {
   const location = useLocation();
@@ -12,9 +13,11 @@ const RSVP = () => {
   const [family, setFamily] = useState(null);
   const [guests, setGuests] = useState([]);
   const [selected, setSelected] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!familySlug) return;
+
     fetch(`/api/invitation?family=${familySlug}`)
       .then((r) => r.json())
       .then((data) => {
@@ -28,6 +31,9 @@ const RSVP = () => {
           initialSelected[g.id] = !!g.accepted;
         });
         setSelected(initialSelected);
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
       });
   }, [familySlug]);
 
@@ -46,43 +52,53 @@ const RSVP = () => {
       body: JSON.stringify({ updates }),
     });
   };
+
   return (
     <div className='bg-1 h-screen flex flex-col justify-center'>
       <img src='./lines_with_birds.webp' className='mb-10 w-[90%] mx-auto' />
-      <h2 className='caligraphy text-4xl text-center mb-2 text-beige-7'>
-        Dobro došli, draga porodico {family?.family_name}
-      </h2>
-      <p className=' text-xl text-center text-beige-6 px-2'>
-        Hvala vam što ste otvorili našu pozivnicu.
-      </p>
-      <p className=' text-xl text-center text-beige-6 leading-4 px-2 my-2'>
-        Vaše prisustvo za nas znači mnogo i iskreno bismo se radovali da ovaj
-        poseban dan podijelimo zajedno.
-      </p>
-      <p className=' text-xl text-center text-beige-6 px-2'>
-        Molimo vas ispod potvrdite svoj dolazak.
-      </p>
-      <ul className='mt-5'>
-        {guests?.map((guest) => (
-          <li key={guest.id} className='ml-[35%] mt-3'>
-            <label className='caligraphy text-beige-6 text-2xl '>
-              <input
-                type='checkbox'
-                checked={!!selected[guest.id]}
-                onChange={() => toggleId(guest.id)}
-                className='mr-3 accent-beige-5'
-              />
-              {guest.full_name}
-            </label>
-          </li>
-        ))}
-      </ul>
-      <button
-        onClick={handleSubmit}
-        className=' text-beige-7 flex mx-auto mt-10 border border-beige-6 px-10 py-1 rounded-md text-2xl'
-      >
-        Potvrdi dolazak
-      </button>
+      {isLoading ? (
+        <p className='w-full text-center text-beige-5 text-7xl caligraphy loading-animation'>
+          A & H
+        </p>
+      ) : (
+        <div className='animate-height'>
+          <h2 className='caligraphy text-4xl text-center mb-2 text-beige-7'>
+            Dobro došli, draga porodico {family?.family_name}
+          </h2>
+          <p className=' text-xl text-center text-beige-6 px-2'>
+            Hvala vam što ste otvorili našu pozivnicu.
+          </p>
+          <p className=' text-xl text-center text-beige-6 leading-4 px-2 my-2'>
+            Vaše prisustvo za nas znači mnogo i iskreno bismo se radovali da
+            ovaj poseban dan podijelimo zajedno.
+          </p>
+          <p className=' text-xl text-center text-beige-6 px-2'>
+            Molimo vas ispod potvrdite svoj dolazak.
+          </p>
+          <ul className='mt-5'>
+            {guests?.map((guest) => (
+              <li key={guest.id} className='ml-[35%] mt-3'>
+                <label className='caligraphy text-beige-6 text-2xl '>
+                  <input
+                    type='checkbox'
+                    checked={!!selected[guest.id]}
+                    onChange={() => toggleId(guest.id)}
+                    className='mr-3 accent-beige-5'
+                  />
+                  {guest.full_name}
+                </label>
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={handleSubmit}
+            className=' text-beige-7 flex mx-auto mt-10 border border-beige-6 px-10 py-1 rounded-md text-2xl'
+          >
+            Potvrdi dolazak
+          </button>
+        </div>
+      )}
+
       <img src='./without_birds.webp' className='mt-10 w-[90%] mx-auto' />
     </div>
   );
