d9072be8389cb7a39317f7124ddd9823fcae4527
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-12-06 16:23:32 +0100

    test rsvp colors

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -47,29 +47,30 @@ const RSVP = () => {
     });
   };
   return (
-    <div className='bg-earth-0 h-screen flex flex-col justify-center'>
-      <h2 className='caligraphy text-4xl text-center mb-2 text-earth-4'>
+    <div className='bg-1 h-screen flex flex-col justify-center'>
+      <img src='./lines_with_birds.webp' className='mb-10 w-[90%] mx-auto' />
+      <h2 className='caligraphy text-4xl text-center mb-2 text-7'>
         Dobro došli, draga porodico {family?.family_name}
       </h2>
-      <p className=' text-xl text-center text-earth-4'>
+      <p className=' text-xl text-center text-6'>
         Hvala vam što ste otvorili našu pozivnicu.
       </p>
-      <p className=' text-xl text-center text-earth-4 leading-3'>
+      <p className=' text-xl text-center text-6 leading-3'>
         Vaše prisustvo za nas znači mnogo i iskreno bismo se radovali da ovaj
         poseban dan podijelimo zajedno.
       </p>
-      <p className=' text-xl text-center text-earth-4'>
+      <p className=' text-xl text-center text-6'>
         Molimo vas ispod potvrdite svoj dolazak.
       </p>
       <ul className='mt-5'>
         {guests?.map((guest) => (
           <li key={guest.id} className='ml-[35%] mt-3'>
-            <label className='caligraphy text-earth-4 text-2xl '>
+            <label className='caligraphy text-6 text-2xl '>
               <input
                 type='checkbox'
                 checked={!!selected[guest.id]}
                 onChange={() => toggleId(guest.id)}
-                className='mr-3 accent-earth-3'
+                className='mr-3 accent-5'
               />
               {guest.full_name}
             </label>
@@ -78,10 +79,11 @@ const RSVP = () => {
       </ul>
       <button
         onClick={handleSubmit}
-        className=' text-earth-4 flex mx-auto mt-10 border-1 px-10 py-1 rounded-md text-2xl'
+        className=' text-5 flex mx-auto mt-10 border-1 px-10 py-1 rounded-md text-2xl'
       >
         Potvrdi dolazak
       </button>
+      <img src='./without_birds.webp' className='mt-10 w-[90%] mx-auto' />
     </div>
   );
 };
