ebba8024af3b7401d1775d13a3436b9aa98fa5f6
hariskablar <125084282+hariskablar@users.noreply.github.com>
2025-12-10 20:37:11 +0100

    steps animated

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,4 +1,4 @@
-import { motion, scale } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const Step = ({ title, time, image }) => {
   return (
@@ -9,7 +9,7 @@ const Step = ({ title, time, image }) => {
       <motion.div
         initial={{ opacity: 0, scale: 0 }}
         whileInView={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5, ease: 'easeIn', delay: 0.2 }}
+        transition={{ duration: 0.7, ease: 'easeIn' }}
         viewport={{ once: true, amount: 0.9 }}
         className='w-[75px] h-[75px] absolute -left-[35px] bg-white p-3 border-2 border-beige-7 rounded-full'
       >
@@ -19,21 +19,21 @@ const Step = ({ title, time, image }) => {
         <motion.p
           initial={{ opacity: 0, x: '30px' }}
           whileInView={{ opacity: 1, x: '0px' }}
-          transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.4 }}
+          transition={{ duration: 1, ease: 'easeIn', delay: 0.2 }}
           viewport={{ once: true, amount: 0.9 }}
           className='caligraphy text-2xl whitespace-pre-line leading-6'
         >
           {title}
         </motion.p>
-        <p
+        <motion.p
           initial={{ opacity: 0, x: '30px' }}
           whileInView={{ opacity: 1, x: '0px' }}
-          transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.5 }}
+          transition={{ duration: 1, ease: 'easeIn', delay: 0.5 }}
           viewport={{ once: true, amount: 0.9 }}
           className='text-xl text-beige-5 leading-4'
         >
           {time}
-        </p>
+        </motion.p>
       </div>
     </div>
   );
